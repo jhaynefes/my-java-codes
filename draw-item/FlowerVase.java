97c2142a26cc9c125e622c1580b9165c61658ce7
Jhaynefe Sollano <148353402+jhaynefes@users.noreply.github.com>
2023-10-19 10:55:21 +0800

    Create FlowerVase.java

---
+++ draw-item/FlowerVase.java
@@ -0,0 +1,29 @@
+package drawItem;
+
+// author @sollano_jhaynefe
+
+public class FlowerVase {
+
+	public static void main(String[] args) {
+		// This will print text art of a vase full of flowers
+		System.out.println("       ");
+		System.out.println("       @(\\/)");
+		System.out.println("     (\\/)-{}-)@");
+		System.out.println("   @(={}=)/\\)(\\/)");
+		System.out.println("  (\\/(/\\)@| (-{}-)");
+		System.out.println(" (={}=)@(\\/)@(/\\)@");
+		System.out.println("  (/\\)\\(={}=)/(\\/)");
+		System.out.println("  @(\\/)\\(/\\)/(={}=)");
+		System.out.println("  (-{}-)\"\"\"\"@/(/\\)");
+		System.out.println("  (-{}-)\"\"\"\"@/(/\\)");
+		System.out.println("   (/\\)|:   |");
+		System.out.println("      /::'   \\");
+		System.out.println("     /:::     \\");
+		System.out.println("    |::'       |");
+		System.out.println("    |::        |");
+		System.out.println("    \\::.       /");
+		System.out.println("     ':______.'");
+		System.out.println("      `\"\"\"\"\"\"`");
+  }
+  
+}
